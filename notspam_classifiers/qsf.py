0ece58c25a1a464592afcac3546f5e11ba8d4291
Jameson Graef Rollins <jrollins@finestructure.net>
2014-12-24 11:01:27 -0800

    fix/enable qsf module

--- notspam_classifiers/qsf.py
+++ notspam_classifiers/qsf.py
@@ -6,9 +6,9 @@ class Learner(NotspamLearner):
     def __init__(self, meat):
         self.cmd = ["qsf", "-a"]
         if meat == 'spam':
-            self.cmd += '--mark-spam'
+            self.cmd += ['--mark-spam']
         elif meat == 'ham':
-            self.cmd += '--mark-nonspam'
+            self.cmd += ['--mark-nonspam']
 
     def add(self, msg):
         with open(msg.get_filename(), 'r') as f:
@@ -32,7 +32,19 @@ class Classifier(NotspamClassifier):
                                     stdout=subprocess.PIPE,
                                     stderr=subprocess.PIPE,
                                     )
-            (stdout, stderr) = proc.communicate()
+            for line in proc.stdout:
+                line = line.decode().split()
+                if line[0] == 'X-Spam:':
+                    print(line)
+                    c = line[1]
+                    break
+            proc.communicate()
             ret = proc.wait()
-        return
-        return isspam, '%s/%s' % (float(score), float(threshold))
+        print(c)
+        if c == 'YES':
+            isspam = True
+        elif c == 'NO':
+            isspam = False
+        else:
+            isspam = None
+        return isspam, ''
